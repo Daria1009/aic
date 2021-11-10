ebf2fcd02d183e71f84ea5f6c9e5072db1dd2ef5
Daria1009 <cryfromtheheart722@gmail.com>
2021-11-10 17:05:05 +0300

    доработка страницы об организации

--- js/app.js
+++ js/app.js
@@ -20,23 +20,20 @@ window.addEventListener('DOMContentLoaded', () => {
     let history = document.querySelector('.information_content_history');
     let structure = document.querySelector('.information_content_structure');
     let receipt = document.querySelector('.information_content_receipt');
-    let btnHistory = document.querySelector('.btn_history');
-    let btnStructure = document.querySelector('.btn_structure');
-    let btnReceipt = document.querySelector('.btn_receipt');
 
-    // btnHistory.addEventListener('click', () => {
-    //     history.style.display = "block";
-    //     structure.style.display = "none";
-    //     receipt.style.display = "none";
-    // });
-    // btnStructure.addEventListener('click', () => {
-    //     history.style.display = "none";
-    //     structure.style.display = "block";
-    //     receipt.style.display = "none";
-    // });
-    // btnReceipt.addEventListener('click', () => {
-    //     history.style.display = "none";
-    //     structure.style.display = "none";
-    //     receipt.style.display = "block";
-    // });
+    document.getElementById('btn_1').addEventListener('click', () => {
+        document.getElementById('content_1').style.display = "block";
+        document.getElementById('content_2').style.display = "none";
+        document.getElementById('content_3').style.display = "none";
+    });
+    document.getElementById('btn_2').addEventListener('click', () => {
+        document.getElementById('content_1').style.display = "none";
+        document.getElementById('content_2').style.display = "block";
+        document.getElementById('content_3').style.display = "none";
+    });
+    document.getElementById('btn_3').addEventListener('click', () => {
+        document.getElementById('content_1').style.display = "none";
+        document.getElementById('content_2').style.display = "none";
+        document.getElementById('content_3').style.display = "block";
+    });
 });
