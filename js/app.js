5bb3a13f5dfd226e89da6d93f44fe50fb5ff1f56
Daria1009 <cryfromtheheart722@gmail.com>
2021-11-10 12:39:34 +0300

    добавление проекта

---
+++ js/app.js
@@ -0,0 +1,20 @@
+window.addEventListener('DOMContentLoaded', () => {
+    let menuOpen = document.querySelector('.menu_open');
+    let lanOpen = document.querySelector('.language_open');
+    let menuLink = document.querySelector('.header_menu');
+    
+    const menuLinkOpen = document.querySelectorAll('.menu_link');
+
+
+    menuOpen.addEventListener('click', () => {
+        menuLink.classList.toggle("open_menu")
+    });
+    lanOpen.addEventListener('click', () => {
+        menuLink.classList.toggle("open_menu")
+    });
+    menuLinkOpen.forEach(item => {
+        item.addEventListener('click', () => {
+            menuLink.classList.toggle("open_menu")
+        })
+    });
+});
