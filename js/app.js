a374bcca3796932555706c4b7672700c018cba38
Daria1009 <cryfromtheheart722@gmail.com>
2021-11-10 16:15:30 +0300

    разработка страницы об организации

--- js/app.js
+++ js/app.js
@@ -5,7 +5,6 @@ window.addEventListener('DOMContentLoaded', () => {
     
     const menuLinkOpen = document.querySelectorAll('.menu_link');
 
-
     menuOpen.addEventListener('click', () => {
         menuLink.classList.toggle("open_menu")
     });
@@ -17,4 +16,27 @@ window.addEventListener('DOMContentLoaded', () => {
             menuLink.classList.toggle("open_menu")
         })
     });
+
+    let history = document.querySelector('.information_content_history');
+    let structure = document.querySelector('.information_content_structure');
+    let receipt = document.querySelector('.information_content_receipt');
+    let btnHistory = document.querySelector('.btn_history');
+    let btnStructure = document.querySelector('.btn_structure');
+    let btnReceipt = document.querySelector('.btn_receipt');
+
+    // btnHistory.addEventListener('click', () => {
+    //     history.style.display = "block";
+    //     structure.style.display = "none";
+    //     receipt.style.display = "none";
+    // });
+    // btnStructure.addEventListener('click', () => {
+    //     history.style.display = "none";
+    //     structure.style.display = "block";
+    //     receipt.style.display = "none";
+    // });
+    // btnReceipt.addEventListener('click', () => {
+    //     history.style.display = "none";
+    //     structure.style.display = "none";
+    //     receipt.style.display = "block";
+    // });
 });
