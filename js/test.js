215b49fdbe7c71288d25a1fec396e11f14f98e03
Daria1009 <cryfromtheheart722@gmail.com>
2021-11-13 03:06:52 +0300

    разработка страниц + исправление багов в меню

--- js/test.js
+++ js/test.js
@@ -1,43 +1,10 @@
 window.addEventListener('DOMContentLoaded', () => {
-    let text1 = document.querySelector('.text_1');
-    let text2 = document.querySelector('.text_2');
-    let text3 = document.querySelector('.text_3');
-    let text4 = document.querySelector('.text_4');
-    let text5 = document.querySelector('.text_5');
+    $( "#tabs" ).tabs();
 
-    document.querySelector('.struc_btn_1').addEventListener('click', () => {
-        text1.style.display = "block";
-        text2.style.display = "none";
-        text3.style.display = "none";
-        text4.style.display = "none";
-        text5.style.display = "none";
-    });
-    document.querySelector('.struc_btn_2').addEventListener('click', () => {
-        text1.style.display = "none";
-        text2.style.display = "block";
-        text3.style.display = "none";
-        text4.style.display = "none";
-        text5.style.display = "none";
-    });
-    document.querySelector('.struc_btn_3').addEventListener('click', () => {
-        text1.style.display = "none";
-        text2.style.display = "none";
-        text3.style.display = "block";
-        text4.style.display = "none";
-        text5.style.display = "none";
-    });
-    document.querySelector('.struc_btn_4').addEventListener('click', () => {
-        text1.style.display = "none";
-        text2.style.display = "none";
-        text3.style.display = "none";
-        text4.style.display = "block";
-        text5.style.display = "none";
-    });
-    document.querySelector('.struc_btn_5').addEventListener('click', () => {
-        text1.style.display = "none";
-        text2.style.display = "none";
-        text3.style.display = "none";
-        text4.style.display = "none";
-        text5.style.display = "block";
-    });
+    if($(window).width() > 992){
+        $('a.link_desk').on("click", function(e) {
+            e.preventDefault(); 
+            location.hash = this.hash;
+        });
+    }
 });
