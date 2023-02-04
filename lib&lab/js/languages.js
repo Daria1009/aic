92fc56d386fae1cd958cee34b1bad44e348940e0
Daria1009 <Sofeina555@mail.ru>
2023-02-04 19:11:28 +0300

    добавление языка + некоторые правки

---
+++ lib&lab/js/languages.js
@@ -0,0 +1,203 @@
+const languages = {
+	"aboutUs": {
+		"ru": "О нас",
+		"en": "About Us",
+	},
+	"services": {
+		"ru": "Услуги",
+		"en": "Services",
+	},
+	"projects": {
+		"ru": "Проекты",
+		"en": "Projects",
+	},
+	"contacts": {
+		"ru": "Контакты",
+		"en": "Contact Us",
+	},
+	"aboutUsTitle": {
+		"ru": "О НАС<span></span>",
+		"en": "About Us<span></span>",
+	},
+	"servicesTitle": {
+		"ru": "Услуги<span></span>",
+		"en": "Services<span></span>",
+	},
+	"projectsTitle": {
+		"ru": "Проекты<span></span>",
+		"en": "Projects<span></span>",
+	},
+	"contactsTitle": {
+		"ru": "Контакты<span></span>",
+		"en": "Contact Us<span></span>",
+	},
+	"partners": {
+		"ru": `Предприниматели – <strong>наши супергерои!</strong> Но даже они встречаются с трудностями в цифровом мире. Мы же телепортируем проекты своих настоящихи будущих друзей в цифру.`,
+		"en": `Entrepreneurs are <strong>our superheroes!</strong> But even they face challenges in the digital world. We teleport projects of our current and future friends to the Digital World.`,
+	},
+	"weDo": {
+		"ru": `Что делаем:
+		<ul>
+			<li>аналитика и концепт IT-продуктов</li>
+			<li><strong>разработка IT-продуктов любой сложности</strong></li>
+			<li>стратегия бренда и маркетинговая стратегия</li>
+			<li>айдентика</li>
+			<li>пользовательский опыт (UX)</li>
+			<li>пользовательский интерфейс (UI)</li>
+		</ul>`,
+		"en": `Our services include:
+		<ul>
+			<li>product concepts</li>
+			<li><strong>IT-products analytics & design</strong></li>
+			<li>IT-products development</li>
+			<li>brand strategy</li>
+			<li>identity</li>
+			<li>digital design (UX/UI)</li>
+			<li>marketing strategy</li>
+		</ul>`,
+	},
+	"processes": {
+		"ru": `НАШИ ПРОЦЕССЫ <br> интервью → исследование → образ решения → аналитика и проектирование → разработка → запуск → сопровождение`,
+		"en": `OUR PROCESSES<br>interview -> research -> solution view -> analytics & design -> develop -> deploy -> maintenance and support`,
+	},
+	"strategy": {
+		"ru": `Мы вовлекаем клиента в рабочий процесс и
+		фокусируемся на <b>стратегических</b> и бизнес-целях,
+		а не на личных вкусах и тенденциях.`,
+		"en": `We involve the Сlient in the workflow and focus on <b>strategic</b> and business goals rather than personal tastes and trends.`,
+	},
+	"processesInscription": {
+		"ru": `ПРОЦЕССЫ`,
+		"en": `PROCESSES`,
+	},
+	"productsInscription": {
+		"ru": `ПРОДУКТЫ`,
+		"en": `products`,
+	},
+	"management": {
+		"ru": `Управление данными и отзывами на картах, в справочниках и социальных сетях из единого кабинета. 
+		
+		<strong>Pako 2022 ©</strong>`,
+		"en": `Manage data and reviews on maps, in directories and social networks from a single cabinet.
+		
+		<strong>Pako 2022 ©</strong>`,
+	},
+	"servicesBtn": {
+		"ru": `<b>Обсудим?</b>`,
+		"en": `<b>Let's’ discuss about this?</b>`,
+	},
+	"marketing": {
+		"ru": `Интернет-маркетинг`,
+		"en": `Internet Marketing`,
+	},
+	"marketingContent": {
+		"ru": `Эффективное продвижение Ваших товаров или услуг онлайн.
+		Настройка и ведение как локальных, так и масштабных кампаний продвижения.
+		Перманентное отслеживание активностей Ваших клиентов на всем пути: от первого
+		взаимодействия до продажи товара или услуги. Точечное таргетирование. Каналы
+		продвижения: реклама (контекстная, таргетированная, медийная) ⚬ SMM (social media
+		marketing) ⚬ SEO-продвижение ⚬ email-маркетинг ⚬ веб-аналитика ⚬
+		контент-маркетинг.`,
+		"en": `Effective online promotion of your products and services. Creation and support of local and large advertising companies. Permanent tracking of your customers' activities on all the way: from the first interaction to the sale of a product or service. Target. Promotion channels: advertising (context, target, media) ⚬ SMM (social media marketing) ⚬ SEO ⚬ Email marketing ⚬ web-analytics ⚬ content-marketing.`,
+	},
+	"ITproducts": {
+		"ru": `R&D IT-продуктов`,
+		"en": `IT-Products R&D`,
+	},
+	"ITproductsContent": {
+		"ru": `Проводим глубокие исследования в области современных и перспективных технологий, технологических разрывов в IT-продуктах, предлагаем технически обоснованные решения. Можем создать Ваш продукт “под ключ” от технической аналитики, концептуального и эскизно-технического проектирования до работающего MVP. А если Вам хочется науки - мы ее любим;) И направим к нашим друзьям в научно-исследовательские лаборатории и полигоны, где Вы сможете разрабатывать вашу технологию. А мы Вас сопроводим в этом пути в формате проектного офиса!`,
+		"en": `We conduct deep research in the field of current and future technologies, technological breaks in IT products, and offer technically-based solutions. 
+		There are Computer Vision, Artificial Neural Networks, Internet of Things (IoT), Big Data technologies we consider in our R&D. We can create your MVP, which includes technical analytics, conceptual and technical design, UI/UX. If your product includes theoretical Science  - we love it;) And it's a pleasure for us to recommend our friends from Research Labs, where you can develop your technology. And we will accompany you along this Science way in the format of a project office!
+		`,
+	},
+	"development": {
+		"ru": `Разработка платформ`,
+		"en": `Sites and Web-platforms development`,
+	},
+	"developmentContent": {
+		"ru": `Используем самые современные инструменты и гибкий подход в работе для создания
+		сайтов и платформ любой сложности. Проанализируем Вашу идею и укажем на слабые
+		места, сильные подчеркнем. Работаем по следующему алгоритму: Интервью → Аналитика
+		→ Проектирование → Написание контента → Создание дизайна → Верстка и
+		программирование → Обучение Заказчика. Дополнительно возможно сопровождение и
+		техническое обслуживание Вашего ресурса.`,
+		"en": `We use modern tools and a flexible approach to create Sites and Web-platforms of any complexity. We work according to the following algorithm: Interview → Analytics → Design → Content → Design creation → Page-proofs and programming → Deploying → Customer training. Additionally, we suggest you support and maintain your resources. `,
+	},
+	"project": {
+		"ru": `Проектный офис`,
+		"en": `Project office`,
+	},
+	"projectContent": {
+		"ru": `Централизуем и структурируем управление цифровыми проектами в организации.
+		Вынесем все хлопоты и заботы по проекту за пределы Вашей компании. Работа в
+		соответствии с передовыми проектными фреймворками, грамотная приоритезация задач,
+		широкий набор компетенций нашей команды – все это Вы получите, работая с нами в
+		рамках данной модели. Дополнительно Вы сможете минимизировать риски, связанные с
+		непомерным раздуванием штата и увеличением бюджетов цифровых проектов.`,
+		"en": `We can help you to build the management system in Digital projects in the organization, including your business extending to the Russian market. We will care about your project  and will take all of the possible worries outside of your company. Working in accordance with advanced project frameworks,  task prioritization, a wide range of competencies of our team - you will get all this by working with us within this business-model. Additionally, you will be able to minimize the risks associated with staff growing and increased budgets for Digital projects.`,
+	},
+	"consulting": {
+		"ru": `Консалтинг в IT`,
+		"en": `IT Consulting`,
+	},
+	"consultingContent": {
+		"ru": `Если у Вас есть вопросы по цифровизации вашего продукта и/или Бизнеса, а также
+		как не пропустить волну цифровой экономики мы с удовольствием ответим на них.
+		Сквозные технологии, такие как: Интернет Вещей, технологии Искусственного
+		Интеллекта, блокчейн и другие, уже здесь и сейчас меняют модели и принципы работы
+		бизнеса, формируют новые социальные и экономические концепции.`,
+		"en": `If you have questions about the digitalization of your product and / or Business, as well as how not to miss the wave of the Digitalization, we will be happy to answer them. Here we can also ensure information to you according to the Russian market and related questions. End-to-end technologies, such as the Internet of Things, Artificial Intelligence technologies, blockchain and others, are already here and now changing the models and principles of business operation and in addition forming new social and economic concepts.`,
+	},
+	"analitics": {
+		"ru": `Исследования/Аналитика`,
+		"en": `Research and Analysis - Product/Business`,
+	},
+	"analiticsContent": {
+		"ru": `Проводим продуктовые, бизнес и рыночные исследования в IT. Работаем в формате как
+		открытых кабинетных исследований на основе доступной информации, так и в формате
+		закрытых интервью и опросов. Делаем обоснованные выводы и рекомендации,
+		используем методы эффективного анализа больших данных.`,
+		"en": `We conduct product, business and market research in IT. We work in the format of both open desk research based on available information, and in the format of closed interviews and polls. We make well-grounded conclusions and recommendations, and use methods of effective big data algorithms.`,
+	},
+	"yourProject": {
+		"ru": `ВАШ ПРОЕКТ__`,
+		"en": `your project__`,
+	},
+	"consultation": {
+		"ru": `Свяжитесь <br> с нами и получите консультацию`,
+		"en": `Contact us <br> and get a consultation`,
+	},
+	"agreement": {
+		"ru": `Нажимая на кнопку, вы даете согласие на обработку <a href="#">персональных данных</a> и
+		соглашаетесь c <a href="#">политикой конфиденциальности</a>`,
+		"en": `By clicking on the button, you are agreeing to the processing of <a href="#">personal data</a> and agree to the <a href="#">privacy policy</a>`,
+	},
+	"formTitle": {
+		"ru": `свяжитесь с нами`,
+		"en": `Contact Us`,
+	},
+	"formCompany": {
+		"ru": `компания`,
+		"en": `company`,
+	},
+	"formPhone": {
+		"ru": `телефон`,
+		"en": `tel`,
+	},
+	"formBtn": {
+		"ru": `отправить`,
+		"en": `send`,
+	},
+	"support": {
+		"ru": `Техническая поддержка:`,
+		"en": `Support:`,
+	},
+	"policy": {
+		"ru": `Политика в отношении обработки персональных данных`,
+		"en": `Privacy Policy Agreement:`,
+	},
+	"digitalWorld": {
+		"ru": `Телепортируем проекты своих настоящих и будущих друзей в цифру`,
+		"en": `We teleport projects of our current and future friends to the Digital World`,
+	},
+}
